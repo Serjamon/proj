07f18e23bf79772cd94072143ed397aa25019d23
Sergey Akimov <rsl2folly@gmail.com>
2024-06-24 20:30:47 +0300

    new class TestClass

--- src/main/java/com/stepup/MainApp.java
+++ src/main/java/com/stepup/MainApp.java
@@ -3,7 +3,8 @@ package com.stepup;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello World 1st!");
         System.out.println("Hello world again!");
+        System.out.println(222);
+        System.out.println(333);
     }
 }
