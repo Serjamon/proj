1f32e826a78939980eb935a5268c324296b86a5e
Sergey Akimov <rsl2folly@gmail.com>
2024-06-24 20:24:21 +0300

    new println

--- src/main/java/com/stepup/MainApp.java
+++ src/main/java/com/stepup/MainApp.java
@@ -3,5 +3,7 @@ package com.stepup;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello World 1st!");
+        System.out.println("Hello world again!");
     }
 }
