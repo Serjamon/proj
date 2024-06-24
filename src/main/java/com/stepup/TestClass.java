7ebbbc26486a39ac3259c8176ea64c1a458c0a39
Sergey Akimov <rsl2folly@gmail.com>
2024-06-24 20:32:20 +0300

    новые принтлн

--- src/main/java/com/stepup/TestClass.java
+++ src/main/java/com/stepup/TestClass.java
@@ -3,5 +3,8 @@ package com.stepup;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Test class");
+
+        System.out.println("2nd commit here");
+        System.out.println("21212");
     }
 }
